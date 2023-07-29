ddbc3c2b06fd726216e654dd0037f24571c6388a
Rameshkumar Dhanvine <rdhanvine321@gmail.com>
2023-07-29 14:47:11 +0800

    added vuetify support and installed dependancies

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import hello from './components/hello.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="AWS Project Website" />
+  <hello/>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
+// main.js
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import "vuetify/dist/vuetify.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app');
