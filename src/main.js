7c507c3d51093141f8032955b97462952c01741a
Suraj Kumar <nehalkumarsuraj@gmail.com>
2023-08-05 15:20:25 +0800

    updated login page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 // main.js
 import { createApp } from 'vue'
-import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.vue';
 import "vuetify/dist/vuetify.css";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
+/* add icons to the library */
+library.add(faUserSecret)
 // Import the router with the correct path
 import router from './plugins/router';
 
--- src/views/loginpage.vue
+++ src/views/loginpage.vue
@@ -5,28 +5,86 @@ import timeline from '../components/timeline.vue'
 </script>
 
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <div >
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
+    >
+    <h1 id="text">Welcome Back!</h1>
+      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+      <v-text-field
+        density="compact"
+        placeholder="Username"
+        prepend-inner-icon="../assets/user.svg"
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Password
+
+        <a
+          class="text-caption text-decoration-none text-blue"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Forgot login password?</a>
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Enter your password"
+        prepend-inner-icon= '{<font-awesome-icon icon="circle-user" style="color: #8c857b;" />}'
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+      <v-btn
+        block
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+      >
+        Log In
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <a
+          class="text-blue text-decoration-none"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
         </a>
-        <a href="https://vuejs.org/" target="_blank">
-        <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-        <v-btn> Login page  </v-btn>
+      </v-card-text>
+    </v-card>
     </div>
 </template>
 
+<script>
+  export default {
+    data: () => ({
+      visible: false,
+    }),
+  }
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#text{
+  color: #A50832;
+  position: fixed;
+  top:381px;
+  left: 60px;
+  width: 178px;
+  height: 28px;
+  font-size: 24px;
+  font-family: helvetica;
+  font-weight: bold;
 }
 </style>
