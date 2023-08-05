542ca4460995b6301be4de8456f7828cf7b9a158
Sato Yuito <satoyuito19@gmail.com>
2023-08-05 14:40:26 +0800

    help

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,12 +1,12 @@
 // router.js
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/homepage.vue'; // Use relative path to the views folder
-import LoginPage from '../views/loginpage.vue'
-import TermsPage from '../views/termspage.vue'
+import LoginPage from '../views/loginpage.vue';
+import TermsPage from '../views/termspage.vue';
 const routes = [
   { path: "/", component: HomePage },
   { path: "/LoginPage", component: LoginPage },
-  { path: "/TermsPage", component: TermsPage}
+  { path: "/TermsPage", component: TermsPage},
 ];
 
 const router = createRouter({
--- src/views/termspage.vue
+++ src/views/termspage.vue
@@ -1,5 +1,8 @@
-<script setup>
-import { RouterLink } from 'vue-router';
-import HelloWorld from '../components/HelloWorld.vue'
-import timeline from '../components/timeline.vue'
-</script>
+
+
+<template>
+    <div>
+        <h1> ffwefwefwf</h1>
+    </div>
+
+</template>
